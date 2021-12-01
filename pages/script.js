0ef7c7ebca2aaa3835a5a59060534b96fcf16d6f
Alexey Gushchin <alekseigushchin@yandex.ru>
2021-12-02 01:11:33 +0500

    fix: исправить замечания

--- pages/script.js
+++ pages/script.js
@@ -1,11 +1,11 @@
 let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.profile__edit-button');
-let closePopup = document.querySelector('.popup__close-button');
+let popupOpen = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__close-button');
 let popupForm = document.querySelector('.popup__form');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-let inputName = popupForm.querySelector('.popup__input-name');
-let inputJob = popupForm.querySelector('.popup__input-job');
+let inputName = popupForm.querySelector('#name-input');
+let inputJob = popupForm.querySelector('#description-input');
 
 function open() {
   popup.classList.add('popup_opened');
@@ -16,14 +16,14 @@ function open() {
 function close() {
   popup.classList.remove('popup_opened');
 }
-openPopup.addEventListener('click', open);
-closePopup.addEventListener('click', close);
 
-function formSubmitHandler(evt) {
+function submitFormHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = inputName.value;
   profileSubtitle.textContent = inputJob.value;
   close();
 }
 
-popupForm.addEventListener('submit', formSubmitHandler);
+popupForm.addEventListener('submit', submitFormHandler);
+popupOpen.addEventListener('click', open);
+popupClose.addEventListener('click', close);
